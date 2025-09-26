37ccdf5fdb49e7172980a2d4ba9d9f858c0888f4
natsuki ueda <63272932+natsuki-engr@users.noreply.github.com>
2025-09-26 07:01:27 +0000

    chore: resolve some error

--- tests/utils/load-theme-configs.ts
+++ tests/utils/load-theme-configs.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import themes from "./theme-projects.json" with { type: "json" };
+import themes from "./theme-projects.json";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
